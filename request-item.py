b66eca3cada7492ab1b9b8129ed0f63201c92938
Russell Pope <rpope@kovarus.com>
2017-04-24 17:13:00 -0700

    created request-item.py to do some testing/validation from API

---
+++ request-item.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+"""
+
+    vRA Request Tool
+
+"""
+
+__version__ = "$Revision"
+
+import getpass
+import argparse
+import vralib
+import json
+
+
+def getargs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--server',
+                        required=True,
+                        action='store',
+                        help='FQDN of the Cloud Provider.')
+    parser.add_argument('-u', '--username',
+                        required=False,
+                        action='store',
+                        help='Username to access the cloud provider')
+    parser.add_argument('-t', '--tenant',
+                        required=True,
+                        action='store',
+                        help='vRealize tenant')
+    parser.add_argument('-c', '--catalogitem',
+                        required=True,
+                        action='store',
+                        help='The partial or full name of the catalog item you want the ID for')
+    args = parser.parse_args()
+    return args
+
+def main():
+    args = getargs()
+    cloudurl = args.server
+    username = args.username
+    tenant = args.tenant
+    if not username:
+        username = raw_input("vRA Username (user@domain):")
+
+    password = getpass.getpass("vRA Password:")
+    vra = vralib.Session.login(username, password, cloudurl,
+                               tenant, ssl_verify=False)
+
+    request_template = vra.get_request_template(catalogitem=args.catalogitem)
+
+    request_template['data']['Linux_vSphere_VM']['data']['Puppet.RoleClass'] = "role::linux_mysql_database"
+
+    build_vm = vra.request_item(catalogitem=args.catalogitem,
+                                payload=request_template)
+
+    print(json.dumps(build_vm, indent=4))
+
+if __name__ == '__main__':
+    main()
